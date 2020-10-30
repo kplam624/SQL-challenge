083b9b13c6083b7f8971b24690efea7c60b2f763
kplam624 <kplam624@gmail.com>
2020-10-29 23:23:40 -0400

    Finished creating tables. Created an image of the erd. Moving towards some analysis.

--- SQL_Queries/tablecreation.sql
+++ SQL_Queries/tablecreation.sql
@@ -15,4 +15,59 @@ hire_date date
 
 -- Checking to see if the import and table are correct
 
-SELECT * FROM employees
+SELECT * FROM employees;
+
+-- Creating a table for Department Employees
+
+CREATE TABLE Department_Employees(
+emp_no int,
+dept_no varchar(10)
+);
+
+-- Checking to see if the import and table are correct
+
+SELECT * FROM Department_Employees;
+
+-- Creating a table for Department
+
+CREATE TABLE Department(
+dept_no varchar(10),
+dept_name varchar(30)
+);
+
+-- Checking to see if the import and table are correct
+
+SELECT * FROM Department;
+
+-- Creating a table for Department Manager
+
+CREATE TABLE Department_Manager(
+dept_no varchar(10),
+emp_no int
+);
+
+-- Checking to see if the import and table are correct
+
+SELECT * FROM Department_Manager;
+
+-- Creating a table for titles
+
+CREATE TABLE Titles(
+title_id varchar(10),
+title varchar(30)
+);
+
+-- Checking to see if the import and table are correct
+
+SELECT * FROM Titles;
+
+-- Creating a table for Salaries
+
+CREATE TABLE Salaries(
+emp_no int, 
+salary int
+);
+
+-- Checking to see if the import and table are correct
+
+SELECT * FROM Salaries;
