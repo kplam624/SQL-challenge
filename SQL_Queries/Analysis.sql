ec168f362984bdf2d8fa5db83faf4eb100467909
kplam624 <kplam624@gmail.com>
2020-11-03 17:57:47 -0500

    Found the sales team.

--- SQL_Queries/Analysis.sql
+++ SQL_Queries/Analysis.sql
@@ -26,9 +26,19 @@ INNER JOIN employees AS e ON dm.emp_no = e.emp_no;
 ----
 
 -- Finding Hercules
+
 CREATE VIEW "Finding Hercules" AS
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' 
 AND last_name LIKE 'B%';
+----
 
+-- Sales Department Employees
+
+CREATE VIEW "Sales Team" AS
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN department_employees AS de ON e.emp_no = de.emp_no
+INNER JOIN department AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
