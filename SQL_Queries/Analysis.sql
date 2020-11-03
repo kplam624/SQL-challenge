88fba209ca9c12f95a2633a7d27db0b05e67c984
kplam624 <kplam624@gmail.com>
2020-11-03 18:22:40 -0500

    Found the employees in Sales and Development

--- SQL_Queries/Analysis.sql
+++ SQL_Queries/Analysis.sql
@@ -53,7 +53,14 @@ WHERE d.dept_name = 'Sales';
 
 -- Finding all employees in the Sales and Development department
 
-SELECT *
-FROM department AS d
-WHERE dept_name = 'Sales' OR dept_name = 'Development';
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN department_employees AS de ON e.emp_no = de.emp_no
+INNER JOIN department AS d ON de.dept_no = d.dept_no
+WHERE dept_name = 'Sales' 
+OR dept_name = 'Development';
+----
 
+-- Frequency of last names
+SELECT *
+FROM employees;
