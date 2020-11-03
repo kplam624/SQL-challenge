14085842054726ce58aceb3cf87d3b148f1b16a5
kplam624 <kplam624@gmail.com>
2020-11-02 23:24:06 -0500

    Added new code for views hire date

--- SQL_Queries/Analysis.sql
+++ SQL_Queries/Analysis.sql
@@ -1,7 +1,14 @@
 -- Queries.
 
 -- Employee details
+
 CREATE VIEW "Employee Overview" AS
 SELECT employees.emp_no, last_name, first_name, sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+-- Employees hired in 1986
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
