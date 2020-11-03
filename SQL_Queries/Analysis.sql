2aad47cd6f8b88a982adf68f7f2be2feaa647344
kplam624 <kplam624@gmail.com>
2020-11-03 17:13:32 -0500

    Found the department managers.

--- SQL_Queries/Analysis.sql
+++ SQL_Queries/Analysis.sql
@@ -6,9 +6,26 @@ CREATE VIEW "Employee Overview" AS
 SELECT employees.emp_no, last_name, first_name, sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
+----
 
 -- Employees hired in 1986
 
+CREATE VIEW "Employees by hire date" AS
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+----
+
+--Employee managers by departments
+
+CREATE VIEW "Department Managers" AS
+SELECT dm.emp_no, e.last_name, e.first_name, d.dept_name
+FROM department AS d
+INNER JOIN department_manager AS dm ON dm.dept_no = d.dept_no
+INNER JOIN employees AS e ON dm.emp_no = e.emp_no;
+----
+
+-- Finding Hercules
+SELECT first_name, last_name, sex
+FROM employees;
+
